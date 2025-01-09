dfcc618504afc0d9c1ac948854b0b25c0833257d
Guiribei <guilherme_f_ribeiro@hotmail.com>
2025-01-08 22:47:48 -0300

    grace exercice done

---
+++ C/sources/Colleen.c
@@ -0,0 +1,20 @@
+/*Bem vindo ao Quine XD*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *build_string(const char *src){
+    char *dest = malloc(strlen(src)+1);
+    if(!dest)return NULL;
+    strcpy(dest, src);
+    return dest;
+}
+
+int main(void){
+    /*Comentario dentro da main*/
+    char *quine = build_string("/*Bem vindo ao Quine XD*/%c%c#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c%cchar *build_string(const char *src){%c    char *dest = malloc(strlen(src)+1);%c    if(!dest)return NULL;%c    strcpy(dest, src);%c    return dest;%c}%c%cint main(void){%c    /*Comentario dentro da main*/%c    char *quine = build_string(%c%s%c);%c    printf(quine,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,quine,34,10,10,10,10,10,10);%c    free(quine);%c    return 0;%c}%c");
+    printf(quine,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,quine,34,10,10,10,10,10,10);
+    free(quine);
+    return 0;
+}
---
+++ C/sources/Grace.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Grace */
+#define S "#include <stdio.h>%c/* Grace */%c#define S %c%s%c%c#define F %cGrace_kid.c%c%c#define M int main(){FILE*fp=fopen(F,%cw%c);if(fp){fprintf(fp,S,10,10,34,S,34,10,34,34,10,34,34,10,10,10);}return 0;}%c%cM%c"
+#define F "Grace_kid.c"
+#define M int main(){FILE*fp=fopen(F,"w");if(fp){fprintf(fp,S,10,10,34,S,34,10,34,34,10,34,34,10,10,10);}return 0;}
+
+M
