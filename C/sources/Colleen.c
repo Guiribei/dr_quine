6895b336ea5698e1d016db657f41c2241df06df1
Guiribei <guilherme_f_ribeiro@hotmail.com>
2025-01-09 23:40:41 -0300

    change commentary style

--- C/sources/Colleen.c
+++ C/sources/Colleen.c
@@ -1,4 +1,6 @@
-/*Bem vindo ao Quine XD*/
+/*
+    Bem vindo ao Quine XD
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,9 +14,11 @@ char *build_string(const char *src){
 }
 
 int main(void){
-    /*Comentario dentro da main*/
-    char *quine = build_string("/*Bem vindo ao Quine XD*/%c%c#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c%cchar *build_string(const char *src){%c    char *dest = malloc(strlen(src)+1);%c    if(!dest)return NULL;%c    strcpy(dest, src);%c    return dest;%c}%c%cint main(void){%c    /*Comentario dentro da main*/%c    char *quine = build_string(%c%s%c);%c    printf(quine,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,quine,34,10,10,10,10,10,10);%c    free(quine);%c    return 0;%c}%c");
-    printf(quine,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,quine,34,10,10,10,10,10,10);
+    /*
+        Comentario dentro da main
+    */
+    char *quine = build_string("/*%c    Bem vindo ao Quine XD%c*/%c%c#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c%cchar *build_string(const char *src){%c    char *dest = malloc(strlen(src)+1);%c    if(!dest)return NULL;%c    strcpy(dest, src);%c    return dest;%c}%c%cint main(void){%c    /*%c        Comentario dentro da main%c    */%c    char *quine = build_string(%c%s%c);%c    printf(quine,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,quine,34,10,10,10,10,10,10);%c    free(quine);%c    return 0;%c}%c");
+    printf(quine,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,quine,34,10,10,10,10,10,10);
     free(quine);
     return 0;
 }
--- C/sources/Grace.c
+++ C/sources/Grace.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* Grace */
-#define S "#include <stdio.h>%c/* Grace */%c#define S %c%s%c%c#define F %cGrace_kid.c%c%c#define M int main(){FILE*fp=fopen(F,%cw%c);if(fp){fprintf(fp,S,10,10,34,S,34,10,34,34,10,34,34,10,10,10);}return 0;}%c%cM%c"
+/*
+    Grace quine
+*/
+#define S "#include <stdio.h>%c/*%c    Grace quine%c*/%c#define S %c%s%c%c#define F %cGrace_kid.c%c%c#define M int main(){FILE*fp=fopen(F,%cw%c);if(fp){fprintf(fp,S,10,10,10,10,34,S,34,10,34,34,10,34,34,10,10,10);}return 0;}%c%cM%c"
 #define F "Grace_kid.c"
-#define M int main(){FILE*fp=fopen(F,"w");if(fp){fprintf(fp,S,10,10,34,S,34,10,34,34,10,34,34,10,10,10);}return 0;}
+#define M int main(){FILE*fp=fopen(F,"w");if(fp){fprintf(fp,S,10,10,10,10,34,S,34,10,34,34,10,34,34,10,10,10);}return 0;}
 
 M
